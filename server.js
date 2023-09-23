0d38f64366a5203399a1ac54cd1bfc55d69149cb
ds.vasudev <vasudevds1729@gmail.com>
2023-09-23 13:22:21 +0530

    updated auth

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express=require("express")
-
+const axios=require("axios");
 
 const app=express();
 
 
-
+let bearerToken;
 app.get("/",(req,res)=>{
+    fetchToken();
     res.send("hello welcome");
 });
+
+
+
+   
+
 
 
 app.post("/register",function(req,res){
@@ -39,9 +45,8 @@ app.post("/register",function(req,res){
     });
 
 });
-app.post("/auth",function(req,res){
+app.get("/auth",function(req,res){
     const authBody={
-        
             "companyName":"Train Central",
             "clientID":"96904f72-17c7-4365-8da1-94feaaede190",
             "clientSecret":"gLCkwdhRXXqcMjRY",
@@ -57,20 +62,52 @@ app.post("/auth",function(req,res){
         },
         body: JSON.stringify(authBody),
         })
-        .then((response) => {
-            if (response.status(200)) {
-            console.log('Data sent successfully');
-            } else {
-            console.error('Failed to send data');
-            }
-        })
-    
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+
+        .then(response => response.json())
+        if(response.ok){
+            const data=response.json();
+            const token=data.access_token;
+            console.log(token);
+        }
+        
+        
     
 
 });
+
+
+app.get("/trains",(req,res)=>{
+    fetchToken();
+    const headers = new Headers({
+        'Authorization': `Bearer ${bearerToken}`,
+        'Content-Type': 'application/json'
+       });
+       
+       // Make the request to the API
+       fetch('http://20.244.56.144/train/trains', {
+        method: 'GET',
+        headers: headers
+       })
+       .then(response => response.json())
+       .then(data => res.send(data))
+       .catch(error => console.error('Error:', error));
+
+})
+app.get("/trains/:trainNo",(req,res)=>{
+    fetchToken();
+    var trainNo=req.params.trainNo;
+    const headers=new Headers({
+        'Authorization': `Bearer ${bearerToken}`,
+        'Content-Type': 'application/json'
+    });
+    fetch(`http://20.244.56.144/train/trains/${trainNo}`, {
+ method: 'GET',
+ headers: headers
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
+})
 
 
 app.listen(3000,function(req,res){
